c19a46089d554a208f157ec428dd9b5b259bacb7
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-08 18:11:37 +0800

    Improved ControlPanel mobile layout

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -4,7 +4,7 @@ import DiceSelector from 'components/DiceSelector';
 function ControlPanel() {
   return (
     <div>
-      <form className="flex space-x-4 lg:flex-col lg:space-x-0 lg:space-y-4">
+      <form className="flex flex-col space-y-4">
         <div className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-2">
           {Object.values(DiceType).map(type => (
             <DiceSelector key={type} type={type} />
