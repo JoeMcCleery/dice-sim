9592adfc036e191055ea07327127c4ffba78f7e6
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-09 14:59:16 +0800

    Update control panel + improve accessibility

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,19 +1,54 @@
 import { DiceType } from 'types/dice';
 import DiceSelector from 'components/DiceSelector';
+import { MouseEvent, useState } from 'react';
 
 function ControlPanel() {
-  return (
-    <div>
-      <form className="flex flex-col space-y-4">
-        <div className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-2">
-          {Object.values(DiceType).map(type => (
-            <DiceSelector key={type} type={type} />
-          ))}
-        </div>
+  const diceTypes = Object.values(DiceType);
+  const defaultValues = [0, 0, 0, 0, 0, 0];
+  const [values, setValues] = useState(defaultValues);
 
-        <input type="submit" value="Roll!" className="btn" />
-      </form>
-    </div>
+  function onChange(type: DiceType, value: number) {
+    setValues(v => {
+      const tempValues = [...v];
+      tempValues[diceTypes.indexOf(type)] = value;
+      return tempValues;
+    });
+  }
+
+  function reset(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    setValues(defaultValues);
+  }
+
+  return (
+    <form className="flex flex-col space-y-4">
+      <div className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-2">
+        {diceTypes.map((type, idx) => (
+          <DiceSelector
+            key={idx}
+            type={type}
+            value={values[idx]}
+            onChange={onChange}
+          />
+        ))}
+      </div>
+
+      <div className="flex flex-col space-y-2">
+        <input
+          type="submit"
+          value="Roll!"
+          className="btn"
+          title="Roll the dice!"
+        />
+        <button
+          className="btn bg-red-500 hover:bg-red-600"
+          onClick={reset}
+          title="Reset dice counts"
+        >
+          Reset
+        </button>
+      </div>
+    </form>
   );
 }
 
--- src/components/DiceSelector.tsx
+++ src/components/DiceSelector.tsx
@@ -1,20 +1,58 @@
+import { MouseEvent } from 'react';
 import { DiceType } from 'types/dice';
 
 type DiceSelectorProps = {
   type: DiceType;
+  value: number;
+  onChange: (type: DiceType, value: number) => void;
 };
 
-function DiceSelector({ type }: DiceSelectorProps) {
+function DiceSelector({ type, value, onChange }: DiceSelectorProps) {
+  const options = Array.from(Array(11).keys());
+
+  function decrease(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    onChange(type, Math.max(value - 1, 0));
+  }
+
+  function increase(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    onChange(type, Math.min(value + 1, options.length - 1));
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <label htmlFor={type}>{type}</label>
-      <select id={type} name={type} className="input">
-        {Array.from(Array(11).keys()).map(count => (
-          <option key={count} value={count}>
-            {count}
-          </option>
-        ))}
-      </select>
+      <div className="flex">
+        <button
+          className="input hidden w-8 rounded-r-none font-bold lg:inline-block"
+          onClick={decrease}
+          title={`Decrease ${type} count`}
+        >
+          -
+        </button>
+        <select
+          id={type}
+          name={type}
+          className="input lg:rounded-none"
+          value={value}
+          onChange={e => onChange(type, parseInt(e.target.value))}
+          title={`Select ${type} count`}
+        >
+          {options.map((option, idx) => (
+            <option key={idx} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <button
+          className="input hidden w-8 rounded-l-none font-bold lg:inline-block"
+          onClick={increase}
+          title={`Increase ${type} count`}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 }
