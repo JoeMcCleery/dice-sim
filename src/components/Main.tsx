a49dc0e5af29e28dee4fcc62a985f16f7d3f0547
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-16 21:00:11 +0800

    Rounded main panel on desktop

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,12 +3,14 @@ import DiceSimulation from './DiceSimulation';
 
 function Main() {
   return (
-    <main className="m-auto grid size-full max-w-screen-lg grid-cols-1 grid-rows-[1fr_auto] overflow-x-hidden md:grid-cols-[1fr_auto] md:grid-rows-1 md:p-4">
-      <div id="container" className="relative min-h-24">
-        <DiceSimulation />
-      </div>
-      <div className="flex w-full flex-col items-center justify-center bg-teal-500 p-4">
-        <ControlPanel />
+    <main className="md:p-4">
+      <div className="m-auto grid size-full max-w-screen-lg grid-cols-1 grid-rows-[1fr_auto] overflow-hidden md:grid-cols-[1fr_auto] md:grid-rows-1 md:rounded">
+        <div id="container" className="relative min-h-24">
+          <DiceSimulation />
+        </div>
+        <div className="flex w-full flex-col items-center justify-center bg-teal-500 p-4">
+          <ControlPanel />
+        </div>
       </div>
     </main>
   );
