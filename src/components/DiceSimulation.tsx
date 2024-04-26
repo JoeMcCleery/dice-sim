0c04be0c6310f00664a0e37580300fae7a273dd1
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-26 17:15:00 +0800

    Improve response time

--- src/components/DiceSimulation.tsx
+++ src/components/DiceSimulation.tsx
@@ -11,6 +11,7 @@ import {
 import { createEnvironment } from 'scripts/environment';
 import SceneComponent from './SceneComponent';
 import { enablePhysics, havokInstance, havokPlugin } from 'scripts/physics';
+import { PhysicsMotionType, Vector3 } from '@babylonjs/core';
 //import { PhysicsActivationControl } from '@babylonjs/core';
 
 function DiceSimulation() {
@@ -39,20 +40,43 @@ function DiceSimulation() {
     // Ignore if simulation stopped
     if (!simulating) return;
 
-    // Check if any dice are still moving
+    // Check if any dice are still active
+    const linearThreshold = 0.001;
+    const angularThreshold = 0.001;
+    const linear = Vector3.Zero();
+    const angular = Vector3.Zero();
     const active = Object.values(DiceType).some(type => {
       return diceContainers[type].some(([, body]) => {
+        // Check state
         const state = havokInstance.HP_Body_GetActivationState(
           body._pluginData.hpBodyId,
         )[1];
-        return state == havokPlugin._hknp.ActivationState.ACTIVE;
+        const awake = state == havokPlugin._hknp.ActivationState.ACTIVE;
+        if (!awake) return false;
+
+        // Check velocity thresholds
+        body.getLinearVelocityToRef(linear);
+        body.getAngularVelocityToRef(angular);
+        return (
+          linear.x > linearThreshold ||
+          linear.y > linearThreshold ||
+          linear.z > linearThreshold ||
+          angular.x > angularThreshold ||
+          angular.y > angularThreshold ||
+          angular.z > angularThreshold
+        );
       });
     });
     if (active) return;
 
-    // No longer simulating as all dice are inactive
+    // Set dice static
+    Object.values(DiceType).forEach(type => {
+      return diceContainers[type].forEach(([, body]) => {
+        body.setMotionType(PhysicsMotionType.STATIC);
+      });
+    });
+    // Stop simulation
     setSimulating(false);
-
     // Calculate simulation result
     calculateResult();
   }
