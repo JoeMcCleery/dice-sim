9c23d5eb0def3411aa729271b86f6ef5bedbddbe
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-29 12:36:11 +0800

    Bold title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="flex items-center justify-center bg-teal-500 p-4">
-      <h1 className="bold text-3xl">3D Dice Simulator</h1>
+      <h1 className="bold text-3xl font-bold">3D Dice Simulator</h1>
     </header>
   );
 }
